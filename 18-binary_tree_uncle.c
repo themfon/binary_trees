cc5d4ec7fc307d1f49f05d76dae84317fe29f625
Mfon <92741946+themfon@users.noreply.github.com>
2022-08-27 04:42:42 +0100

    binary tree

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,20 +1,16 @@
-#include "17-binary_tree_sibling.c"
+#include "binary_trees.h"
 
 /**
- * binary_tree_uncle -  a function that finds the uncle of a node
- * @node: a pointer to the node to find the uncle
- * Return: a pointer to the uncle node, and NULL If node is NULL or
- * node has no uncle
+ * binary_tree_uncle - finds the uncle of a node
+ * @node: pointer to the node to find the uncle of
+ *
+ * Return: pointer to the uncle node, or NULL if no uncle
  */
-
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	binary_tree_t *parent = NULL;
-	binary_tree_t *sibiling = NULL;
-
-	if (!node || !node->parent || !node->parent->parent)
+	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
 		return (NULL);
-	parent = node->parent;
-	sibiling = binary_tree_sibling(parent);
-	return (sibiling);
+	if (node->parent == node->parent->parent->left)
+		return (node->parent->parent->right);
+	return (node->parent->parent->left);
 }
