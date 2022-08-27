2f9e0607d252cc7fd84dd40d340d6023c974c0aa
Mfon <92741946+themfon@users.noreply.github.com>
2022-08-27 04:29:23 +0100

    binary tree

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,17 +1,23 @@
+#include <stdio.h>
 #include "binary_trees.h"
 
 /**
- * binary_tree_nodes -  a function that counts the nodes with
- * at least 1 child in a binary tree
- * @tree:  a pointer to the root node of the tree to count the number of nodes
- * Return: 0 if tree is NULL, otherwise number of nodes with at least 1 child
+ * binary_tree_nodes - this counts the nodes with at least 1 child
+ * @tree: pointer to the root node of the tree
+ * Return: count
  */
 
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	if (!tree)
+	int left, right, count;
+
+	if (tree == NULL)
 		return (0);
-	if (!tree->left && !tree->right)
+	if (tree->left == NULL && tree->right == NULL)
 		return (0);
-	return (1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
+	left = binary_tree_nodes(tree->left);
+	right = binary_tree_nodes(tree->right);
+
+	count = left + right + 1;
+	return (count);
 }
