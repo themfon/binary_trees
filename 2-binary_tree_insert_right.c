6994b1a2fa22f6cdb9758f19def64845e3603529
Mfon <92741946+themfon@users.noreply.github.com>
2022-08-27 04:12:36 +0100

    binary tree

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,36 +1,32 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_right - a function that inserts a node
- * as the right-child of another node
- * @parent: a pointer to the node to insert the right-child in
- * @value:  the value to store in the new node
- * Return: a pointer to the created node,
- * or NULL on failure or if parent is NULL
+ * binary_tree_insert_right - this function creates a binary tree node
+ * @parent: pointer to the parent node of the node to create
+ * @value: value to put in the new node
+ * Return: pointer to the new node, or NULL on failure
  */
-
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_right;
+	binary_tree_t *nodo = NULL;
 
-	new_right = malloc(sizeof(binary_tree_t));
-	if (!new_right)
+	if (parent == NULL)
 		return (NULL);
-	new_right->n = value;
-	new_right->left = NULL;
-	new_right->right = NULL;
 
-	if (!parent)
-	{
-		free(new_right);
+	nodo = binary_tree_node(parent, value);
+	if (nodo == NULL)
 		return (NULL);
-	}
-	new_right->parent = parent;
-	if (parent->right)
+
+	if (parent->right == NULL)
 	{
-		new_right->right = parent->right;
-		new_right->right->parent = new_right;
+		parent->right = nodo;
 	}
-	parent->right = new_right;
-	return (new_right);
+	else
+	{
+		nodo->right = parent->right;
+		parent->right = nodo;
+		nodo->right->parent = nodo;
+	}
+
+	return (nodo);
 }
