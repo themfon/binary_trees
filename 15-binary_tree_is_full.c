8498a7d78a94d991154668e52d63b7f861edbf1f
Mfon <92741946+themfon@users.noreply.github.com>
2022-08-27 04:35:31 +0100

    binary tree

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,21 +1,35 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_full -  a function that checks if a binary tree is full
- * @tree:  a pointer to the root node of the tree to check
- * Return: 0 if tree is NUL and unbalanced, 1 if tree is balanced
+ * binary_tree_is_leaf - checks if a node is a leaf
+ * @node: pointer to the node to check
+ *
+ * Return: 1 if node is a leaf, and 0 otherwise. If node is NULL, return 0
  */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node != NULL && node->left == NULL && node->right == NULL)
+		return (1);
+	return (0);
+}
 
+/**
+ * binary_tree_is_full -  function checks if a binary tree is full
+ * @tree: is a pointer to the root node of the tree to check
+ * Return: 1 if full otherwise o
+ */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-	if (!tree)
+	int leftSubTree, rightSubTree;
+
+	if (tree == NULL)
 		return (0);
-	if (!tree->left && !tree->right)
+	if (tree->left == NULL && tree->right == NULL)
 		return (1);
-	if (tree->left && tree->right)
-	{
-		if (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right))
-			return (1);
-	}
-	return (0);
+	leftSubTree = binary_tree_is_full(tree->left);
+	rightSubTree = binary_tree_is_full(tree->right);
+
+	if (leftSubTree == 0 || rightSubTree == 0)
+		return (0);
+	return (1);
 }
