1b69b93844db1823e0e0426c32e09c977e1bd291
Mfon <92741946+themfon@users.noreply.github.com>
2022-08-27 04:27:57 +0100

    binary tree

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,17 +1,23 @@
+#include <stdio.h>
 #include "binary_trees.h"
 
 /**
- * binary_tree_leaves - a function that counts the leaves in a binary tree
- * @tree: a pointer to the root node of the tree to count the number of leaves
- * Return: 0 if tree is NULL, otherwise number of leaves
+ * binary_tree_leaves - this counts the leaves in a binary tree
+ * @tree: pointer to the root node of the tree
+ * Return: count
  */
 
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	if (!tree)
-		return (0);
+	int left, right, count;
 
-	if (!tree->left && !tree->right)
+	if (tree == NULL)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
 		return (1);
-	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+	left = binary_tree_leaves(tree->left);
+	right = binary_tree_leaves(tree->right);
+
+	count = left + right;
+	return (count);
 }
