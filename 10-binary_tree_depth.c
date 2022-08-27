64e9b5dace35f8b836e78eebbebdf97f5186fc4a
Mfon <92741946+themfon@users.noreply.github.com>
2022-08-27 04:10:45 +0100

    binary tree

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,22 +1,18 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_depth - a function that measures the depth
- * of a node in a binary tree
- * @tree: a pointer to the node to measure the depth
- * Return: 0 If tree is NULL, otherwise depth of the tree.
+ * binary_tree_depth - this measures the depth of a binary tree
+ * @tree:  pointer to the root node of the tree to measure the height
+ * Return: If tree is NULL, your function must return 0
  */
-
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	size_t d = 0;
+	size_t depth = 0;
 
-	if (!tree)
+	if (tree == NULL || (tree->parent == NULL))
 		return (0);
-	if (tree->parent)
-	{
-		d += binary_tree_depth(tree->parent);
-		d++;
-	}
-	return (d);
+
+	depth = binary_tree_depth(tree->parent);
+
+	return (depth + 1);
 }
