efb6168319c500f025fd19e154426a3a9af59d57
Mfon <92741946+themfon@users.noreply.github.com>
2022-08-27 04:14:16 +0100

    binary tree

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,16 +1,15 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_delete - a function that deletes an entire binary tree
- *
- * @tree:  a pointer to the root node of the tree to delete
- * Return: nothing
+ * binary_tree_delete - this function deletes tree
+ * @tree: pointer to the parent node of the node to delete
+ * Return: Nothing
  */
-
 void binary_tree_delete(binary_tree_t *tree)
 {
-	if (!tree)
+	if (tree == NULL)
 		return;
+
 	binary_tree_delete(tree->left);
 	binary_tree_delete(tree->right);
 	free(tree);
