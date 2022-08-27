65e3137ed8c42feb3a7fca8a54e3eb1589996038
Mfon <92741946+themfon@users.noreply.github.com>
2022-08-27 04:25:08 +0100

    binary tree

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,21 +1,20 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_size - a function that measures the size of a binary tree
- * @tree: a pointer to the root node of the tree to measure the size
- * Return: 0 if tree is NULL, otherwise size of the tree.
+ * binary_tree_size - this measures the height of a binary tree
+ * @tree:  pointer to the root node of the tree to measure the height
+ * Return: If tree is NULL, your function must return 0
  */
-
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	size_t s = 1;
+	size_t left = 0;
+	size_t right = 0;
 
-	if (!tree)
+	if (tree == NULL)
 		return (0);
-	if (tree->left || tree->right)
-	{
-		s += binary_tree_size(tree->left);
-		s += binary_tree_size(tree->right);
-	}
-	return (s);
+
+	left = binary_tree_size(tree->left);
+	right = binary_tree_size(tree->right);
+
+	return (left + right + 1);
 }
