22ae7bd663f061c4cdb78c81369bce19ff2f1f6c
Mfon <92741946+themfon@users.noreply.github.com>
2022-08-27 04:37:11 +0100

    binary tree

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,44 +1,39 @@
-#include <stdio.h>
 #include "binary_trees.h"
 
 /**
- * binary_tree_balance - this measures the balance factor of a binary tree
- * @tree: pointer to the root node of the tree
- * Return: balance factor If tree is NULL, return 0
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: pointer to the root node of the tree to measure the height of
+ * Return: the height of the tree. If tree is NULL, return 0
  */
-
-int binary_tree_balance(const binary_tree_t *tree)
+size_t binary_tree_height(const binary_tree_t *tree)
 {
-	int left, right, count;
+	size_t left, right;
 
 	if (tree == NULL)
 		return (0);
-	if (tree->left == NULL && tree->right == NULL)
-		return (0);
-	left = aux_tree_height(tree->left);
-	right = aux_tree_height(tree->right);
-
-	count = left - right;
-	return (count);
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
+	if (left >= right)
+		return (1 + left);
+	return (1 + right);
 }
 
 /**
- * aux_tree_height - the height of binary tree
- * @tree: pointer to the root node of the tree
- * Return: height
+ * binary_tree_balance - function measures the balance factor
+ * of a binary tree
+ * @tree: is a pointer to the root node of the tree to measur
+ * the balance factor
+ *
+ * Return: an interger to that is equal to 0 for a balnced
+ * factor.
  */
-
-size_t aux_tree_height(const binary_tree_t *tree)
+int binary_tree_balance(const binary_tree_t *tree)
 {
-	int height_left, height_right;
+	int left_sub_tree, right_sub_tree;
 
 	if (tree == NULL)
 		return (0);
-	height_left = aux_tree_height(tree->left) + 1;
-	height_right = aux_tree_height(tree->right) + 1;
-
-	if (height_left > height_right)
-		return (height_left);
-	else
-		return (height_right);
+	left_sub_tree = binary_tree_height(tree->left);
+	right_sub_tree = binary_tree_height(tree->right);
+	return (left_sub_tree - right_sub_tree); /*value < 1 or = 0 is HB(K) bt*/
 }
