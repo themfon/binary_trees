b0ab8495c012e7ef552790f7bf01ff9e9e798230
Mfon <92741946+themfon@users.noreply.github.com>
2022-08-27 04:18:23 +0100

    binary tree

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,17 +1,16 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_preorder - a function that goes through
- * a binary tree using pre-order traversal
- * @tree: a pointer to the root node of the tree to traverse
- * @func: a pointer to a function to call for each node.
- * Return: nothing
+ * binary_tree_preorder - this function creates a binary tree node
+ * @tree: pointer to the parent node of the node to create
+ * @func: pointer of function
+ * Return: pointer to the new node, or NULL on failure
  */
-
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (!tree || !func)
+	if (tree == NULL || func == NULL)
 		return;
+
 	func(tree->n);
 	binary_tree_preorder(tree->left, func);
 	binary_tree_preorder(tree->right, func);
