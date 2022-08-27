1eebb6335ac15f66eade1cb83ea890096605df95
Mfon <92741946+themfon@users.noreply.github.com>
2022-08-27 04:15:35 +0100

    binary tree

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,16 +1,17 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_leaf - a function that checks if a node is a leaf
- * @node: a pointer to the node to check
- * Return: 1 if node is a leaf, otherwise 0
+ * binary_tree_is_leaf - the function that creates a binary tree node
+ * @node: pointer to the parent node of the node to create
+ * Return: pointer to the new node, or NULL on failure
  */
-
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if (!node)
+	if (node == NULL)
 		return (0);
-	if (!node->left && !node->right)
+
+	if (node->left != NULL || node->right != NULL)
+		return (0);
+	else
 		return (1);
-	return (0);
 }
