9d5990093d57e5794aad0ee07c0ed6134ed36d49
Mfon <92741946+themfon@users.noreply.github.com>
2022-08-27 04:41:03 +0100

    binary tree

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -1,24 +1,19 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_sibling - a function that finds the sibling of a node
- * @node: a pointer to the node to find the sibling
- * Return: a pointer to the sibling node, NULL if node has no sibiling
- * or node is NULL or the parent is NULL
+ * binary_tree_sibling - finds the sibling of a node
+ * @node: is a pointer to the node to find the sibling
+ * Return: a pointer to the sibling of the node
  */
-
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	binary_tree_t *new;
-
 	if (!node || !node->parent)
 		return (NULL);
 
-	new = node->parent;
-
-	if (node == new->left)
-	{
-		return (new->right);
-	}
-	return (new->left);
+	return (
+			node->parent->left == node
+			? node->parent->right :
+			node->parent->right == node
+			? node->parent->left : NULL
+		);
 }
